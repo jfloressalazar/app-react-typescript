8e75abc56a59c2f16f64839f2ca9aeec66be9d0f
Jesus Flores <jfloressalazar91@gmail.com>
2025-04-29 18:56:21 -0600

    Clase 11, importe de express en index.js instancia  de express, introduccion al routing,  inicializacion de servidor con puerto personalizado

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
-console.log("Hello, World!");
-console.log("This is a test.");
+// importar express
+const express = require('express');
+
+// inicializar express
+const app = express()
+
+// variable para mi puerto de servidor
+const port = 8080
+
+// Routing
+
+// ruta raiz
+app.get('/', (req, res) => {
+    res.send('Hola  mundo desde Express!')
+})
+
+// ruta a blog
+app.get('/blog', (req, res) => {
+    res.send('Bienvenido a mi blog!')
+})
+// ruta a ecommerce
+app.get('/ecommerce', (req, res) => {
+    res.send('Bienvenido a mi ecommerce!')
+})
+// ruta a contacto
+app.get('/contacto', (req, res) => {
+    res.send('Bienvenido a mi pagina de contacto!')
+})
+
+
+// arrancar el servidor usando el puerto 8080
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+})
