33d680118cad77429e79db146ee77f4b234d80b3
Jesus Flores <jfloressalazar91@gmail.com>
2025-04-30 20:59:19 -0600

    clase 18, definición de rutas de autenticacion

--- src/router.ts
+++ src/router.ts
@@ -1,22 +1,10 @@
 // Este archivo se encarga de definir las rutas de la aplicación
 import { Router } from 'express'
-const router = Router() 
+const router = Router()
 
-router.get('/', (req, res) => {
-    res.send('Hola  mundo desde Express!')
-})
-
-// ruta a blog
-router.get('/blog', (req, res) => {
-    res.send('Bienvenido a mi blog!')
-})
-// ruta a ecommerce
-router.get('/ecommerce', (req, res) => {
-    res.send('Bienvenido a mi ecommerce!')
-})
-// ruta a contacto
-router.get('/contacto', (req, res) => {
-    res.send('Bienvenido a mi pagina de contacto!')
+// rutas para autenticación y registro
+router.post('/auth/register', (req, res) => {
+    console.log('Desde register...')
 })
 
 export default router
