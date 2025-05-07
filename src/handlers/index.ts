5e7f205b6e7c4c30cc6606e83ea894374499f380
Jesus Flores <jfloressalazar91@gmail.com>
2025-05-06 22:34:50 -0600

    Clase 33, creacion de un archivo de handler que servirá para manejar o definir las funciones que servirán para manejar los registros, estas funciones debe ser exportadas e importadas dentro del archivo de rutas ya que en cada ruta se llamará una función especifica

---
+++ src/handlers/index.ts
@@ -0,0 +1,15 @@
+import User from "../models/User"
+
+// Archivo para manejar las funciones que se llaman desde el router
+export const createAccount = async (req, res) => {
+    console.log(req.body)
+    // ipcion 1 nsertar el usuario en la base de datos con el apartado body de la petición
+    // await User.create(req.body)
+
+    // opcion 2 crear una instancia del modelo de usuario con el body de la petición
+    const usuario = new User(req.body)
+    await usuario.save()
+
+    // uso de response para enviar un mensaje de registro exitoso
+    res.send('registro exitoso')
+}
