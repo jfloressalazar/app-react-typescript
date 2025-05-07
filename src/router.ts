e5a3b3a38f91eedbe7de717ced849fd1192c04f1
Jesus Flores <jfloressalazar91@gmail.com>
2025-05-06 22:27:28 -0600

    Clase 32, uso de respuesta para envio de mensaje de confirmacion de registro exitoso al usuario

--- src/router.ts
+++ src/router.ts
@@ -8,11 +8,14 @@ router.post('/auth/register', async (req, res) => {
     console.log(req.body)
 
     // iopcion 1 nsertar el usuario en la base de datos con el apartado body de la petición
-    await User.create(req.body)
+    // await User.create(req.body)
 
     // opcion 2 crear una instancia del modelo de usuario con el body de la petición
-    // const usuario = new User(req.body)
-    // await usuario.save()
+    const usuario = new User(req.body)
+    await usuario.save()
+
+    // uso de response para enviar un mensaje de registro exitoso
+    res.send('registro exitoso')
 
 })
 
