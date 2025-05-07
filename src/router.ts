9e289045ee86620b79fee746e999c7c94c2e7402
Jesus Flores <jfloressalazar91@gmail.com>
2025-05-06 22:22:45 -0600

    Clase 31, almacenar registro de usuario en la base de datos, uso de directo de funcion en la ruta, importacion de modelo y uso de funcion de orm mongoose para la creacion de uusario usando el request.body

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,19 @@
 // Este archivo se encarga de definir las rutas de la aplicación
 import { Router } from 'express'
+import User from './models/User'
 const router = Router()
 
 // rutas para autenticación y registro
-router.post('/auth/register', (req, res) => {
+router.post('/auth/register', async (req, res) => {
     console.log(req.body)
+
+    // iopcion 1 nsertar el usuario en la base de datos con el apartado body de la petición
+    await User.create(req.body)
+
+    // opcion 2 crear una instancia del modelo de usuario con el body de la petición
+    // const usuario = new User(req.body)
+    // await usuario.save()
+
 })
 
 export default router
