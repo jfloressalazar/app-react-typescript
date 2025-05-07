79d3ae0503aae6cbc7a8691b48fa7de9067d60d2
Jesus Flores <jfloressalazar91@gmail.com>
2025-05-06 21:39:23 -0600

    Clase 30, completado de modelo de User, incorporacion de interface para definir el tipado del modelo, ya sea que se utilice una interface o type para la definicion de los tipos este debe ser un 'espejo' del schema

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,13 @@
 import mongoose, { Schema } from "mongoose";
 
+// definimos la interfaz de usuario para el modelo de usuario
+export interface UserInterface {
+    name: string;
+    email: string;
+    username: string;
+    password: string;
+}
+
 //definiendo el esquema de usuario o estructura de la base de datos 
 const userSchema = new Schema({
     name: {
@@ -26,7 +34,7 @@ const userSchema = new Schema({
     },
 })
 
-//definiendo el Modelo de usuario
-const User = mongoose.model("User", userSchema)
+//definiendo el Modelo de usuario y agregando el interface de usuario por medio de generics
+const User = mongoose.model<UserInterface>("User", userSchema)
 // Exportando el modelo de usuario
 export default User
